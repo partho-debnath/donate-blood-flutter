34e6705fb420f399c843ae3357b6b7881676739d
Partho Debnath <parthodebnath28@gmail.com>
2024-07-11 01:44:41 +0600

    registration screen UI updated

--- lib/presentation/ui/screens/registration_screen.dart
+++ lib/presentation/ui/screens/registration_screen.dart
@@ -1,4 +1,5 @@
-import 'package:blooddonate/presentation/ui/screens/login_screen.dart';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -42,6 +43,49 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   // crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Flexible(
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'First Name',
+                              prefixIcon: Icon(Icons.person),
+                            ),
+                            validator: (value) {
+                              if (value?.isEmpty ?? true) {
+                                return 'Enter your first name.';
+                              }
+                              return null;
+                            },
+                            onSaved: (newValue) {},
+                            keyboardType: TextInputType.text,
+                            textInputAction: TextInputAction.next,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Flexible(
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Last Name',
+                              prefixIcon: Icon(Icons.person),
+                            ),
+                            validator: (value) {
+                              if (value?.isEmpty ?? true) {
+                                return 'Enter your last name.';
+                              }
+                              return null;
+                            },
+                            onSaved: (newValue) {},
+                            keyboardType: TextInputType.text,
+                            textInputAction: TextInputAction.next,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
                     TextFormField(
                       keyboardType: TextInputType.emailAddress,
                       textInputAction: TextInputAction.next,
@@ -63,6 +107,129 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                         }
                         return null;
                       },
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Flexible(
+                          child: DropdownButtonFormField(
+                            hint: const Text('Gender'),
+                            items: <DropdownMenuItem>[
+                              DropdownMenuItem(
+                                value: 'm',
+                                child: RichText(
+                                  text: const TextSpan(
+                                    style: TextStyle(color: Colors.black),
+                                    text: 'Male ',
+                                    children: <InlineSpan>[
+                                      WidgetSpan(
+                                        child: Icon(Icons.male),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              DropdownMenuItem(
+                                value: 'f',
+                                child: RichText(
+                                  text: const TextSpan(
+                                    style: TextStyle(color: Colors.black),
+                                    text: 'Female ',
+                                    children: <InlineSpan>[
+                                      WidgetSpan(
+                                        child: Icon(Icons.female),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                            onChanged: (value) {
+                              log(value);
+                            },
+                            validator: (value) {
+                              if (value?.isEmpty ?? true) {
+                                return 'Select your gender';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              log('Gender $value');
+                            },
+                          ),
+                        ),
+                        const SizedBox(width: 5),
+                        Flexible(
+                          child: DropdownButtonFormField(
+                            hint: RichText(
+                              text: const TextSpan(
+                                style: TextStyle(
+                                  color: Colors.black38,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                children: [
+                                  TextSpan(
+                                    text: "Blood ",
+                                  ),
+                                  WidgetSpan(
+                                    child: Icon(
+                                      Icons.bloodtype_outlined,
+                                      color: Colors.red,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            items: const <DropdownMenuItem>[
+                              DropdownMenuItem(
+                                value: 'a+',
+                                child: Text('A Positive'),
+                              ),
+                              DropdownMenuItem(
+                                value: 'a-',
+                                child: Text('A Negative'),
+                              ),
+                            ],
+                            onChanged: (value) {
+                              log(value);
+                            },
+                            validator: (value) {
+                              if (value?.isEmpty ?? true) {
+                                return 'Select your blood group';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              log('blood $value');
+                            },
+                          ),
+                        ),
+                        const SizedBox(width: 5),
+                        Flexible(
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              hintText: 'Date&Birth',
+                              // labelText: 'YY/MM/DD',
+                              suffixIcon: IconButton(
+                                onPressed: () {
+                                  showDatePicker(
+                                    context: context,
+                                    firstDate: DateTime(2000),
+                                    lastDate: DateTime(2024),
+                                    initialDatePickerMode: DatePickerMode.year,
+                                    initialEntryMode:
+                                        DatePickerEntryMode.calendar,
+                                  ).then((onValue) {
+                                    log(onValue.toString());
+                                  });
+                                },
+                                icon: const Icon(Icons.arrow_downward),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                     const SizedBox(height: 10),
                     TextFormField(
@@ -141,7 +308,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                       onPressed: () {
                         if (form.currentState!.validate() == true) {
                           form.currentState!.save();
-                          form.currentState!.reset();
+                          // form.currentState!.reset();
                         } else {
                           print('-----');
                         }

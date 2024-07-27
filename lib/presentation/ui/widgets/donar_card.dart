5875a7b8da1d60f70cf9f767c52e6e3fa57ac147
Partho Debnath <parthodebnath28@gmail.com>
2024-07-27 21:13:17 +0600

    HomeScreen DonarCard's InkWell ripple effect fixed

--- lib/presentation/ui/widgets/donar_card.dart
+++ lib/presentation/ui/widgets/donar_card.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../../../applications/app_colors.dart';
 import '../../../models/donar_model_data.dart';
-import 'donar_detail_screen.dart';
+import './donar_detail_screen.dart';
 
 class DonarCard extends StatelessWidget {
   final DonarData donarData;
@@ -13,101 +14,105 @@ class DonarCard extends StatelessWidget {
       height: 90,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(5),
-        color: const Color(0xffff0000).withOpacity(0.06),
+        color: AppColor.colorSwatch[50],
       ),
       margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-      child: InkWell(
+      child: Material(
+        color: Colors.transparent,
         borderRadius: BorderRadius.circular(5),
-        onTap: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) {
-                return const DonarDetailScreen();
-              },
-            ),
-          );
-        },
-        splashColor: Colors.red.shade100,
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.network(
-                  '${donarData.image}',
-                  // 'https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=1380&t=st=1718523845~exp=1718524445~hmac=04fdacfddd221059842eada08e3e6330c3528bffb00f4a15d77a8556b1bb8045',
-                  height: 80,
-                  width: 80,
-                  fit: BoxFit.cover,
-                  errorBuilder: (context, error, stackTrace) {
-                    return Container(
-                      color: Colors.white70,
-                      height: 80,
-                      width: 80,
-                      child: const Icon(Icons.person_2_outlined),
-                    );
-                  },
-                ),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(5),
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) {
+                  return const DonarDetailScreen();
+                },
               ),
-              Container(
-                margin: const EdgeInsets.only(left: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Text>[
-                    Text(
-                      donarData.fullName,
-                      style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+            );
+          },
+          splashColor: AppColor.colorSwatch[200],
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(
+                    '${donarData.image}',
+                    // 'https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=1380&t=st=1718523845~exp=1718524445~hmac=04fdacfddd221059842eada08e3e6330c3528bffb00f4a15d77a8556b1bb8045',
+                    height: 80,
+                    width: 80,
+                    fit: BoxFit.cover,
+                    errorBuilder: (context, error, stackTrace) {
+                      return Container(
+                        color: Colors.white70,
+                        height: 80,
+                        width: 80,
+                        child: const Icon(Icons.person_2_outlined),
+                      );
+                    },
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Text>[
+                      Text(
+                        donarData.fullName,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    Text('${donarData.address}'),
-                    Text('ID: ${donarData.id}'),
+                      Text('${donarData.address}'),
+                      Text('ID: ${donarData.id}'),
+                    ],
+                  ),
+                ),
+                const Spacer(),
+                Column(
+                  children: <Widget>[
+                    Spacer(),
+                    Text('Donation 1'),
+                    Text('Age 26'),
+                    Text('${donarData.gender}'),
                   ],
                 ),
-              ),
-              const Spacer(),
-              Column(
-                children: [
-                  Spacer(),
-                  Text('Donation 1'),
-                  Text('Age 26'),
-                  Text('${donarData.gender}'),
-                ],
-              ),
-              const SizedBox(width: 10),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Flexible(
-                    child: Card(
-                      margin: const EdgeInsets.all(0.0),
-                      child: Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: Text(
-                          '${donarData.bloodGroup}',
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.red,
+                const SizedBox(width: 10),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Flexible(
+                      child: Card(
+                        margin: const EdgeInsets.all(0.0),
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Text(
+                            '${donarData.bloodGroup}',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.red,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  Flexible(
-                    child: IconButton.filledTonal(
-                      onPressed: () {
-                        print('-----');
-                      },
-                      icon: const Icon(Icons.person_add_alt),
+                    Flexible(
+                      child: IconButton.filledTonal(
+                        onPressed: () {
+                          print('-----');
+                        },
+                        icon: const Icon(Icons.person_add_alt),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),

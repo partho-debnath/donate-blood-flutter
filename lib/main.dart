a4eca173c6ce3629beb52f00a1e72a6419c541b5
Partho Debnath <parthodebnath28@gmail.com>
2024-08-18 15:19:04 +0600

    Locking device orientation as portraitUp

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import './applications/blood_donate.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const BloodDonate());
 }
